718fa14dfa3c9114d6b5d284ba54901439b8f2fb
shahariyar Nafis <72545687+shahariyar77@users.noreply.github.com>
2020-11-30 19:38:54 +0600

    Add files via upload

---
+++ 106-hero-area-carousel/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $(".homepage-slide").owlCarousel({
+   loop:true,
+   nav:true,
+  items:1,
+  navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+autoplay:true,
+     dots:false
+     
+ });
+    
+});
+
+
+
